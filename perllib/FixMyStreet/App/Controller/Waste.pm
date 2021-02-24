21b90ff10580f7469b4b90021075dd7adf3a0bad
Dave Arter <davea@mysociety.org>
2021-02-24 15:34:09 +0000

    [Waste] Hide the quantity field if max containers is one

--- perllib/FixMyStreet/App/Controller/Waste.pm
+++ perllib/FixMyStreet/App/Controller/Waste.pm
@@ -146,7 +146,7 @@ sub construct_bin_request_form {
                 type => 'Checkbox',
                 apply => [
                     {
-                        when => { "quantity-$id" => sub { $_[0] > 0 } },
+                        when => { "quantity-$id" => sub { $max > 1 && $_[0] > 0 } },
                         check => qr/^1$/,
                         message => 'Please tick the box',
                     },
@@ -156,19 +156,26 @@ sub construct_bin_request_form {
                 tags => { toggle => "form-quantity-$id-row" },
             };
             $name = ''; # Only on first container
-            push @$field_list, "quantity-$id" => {
-                type => 'Select',
-                label => 'Quantity',
-                tags => {
-                    hint => "You can request a maximum of " . NUMWORDS($max) . " containers",
-                    initial_hidden => 1,
-                },
-                options => [
-                    { value => "", label => '-' },
-                    map { { value => $_, label => $_ } } (1..$max),
-                ],
-                required_when => { "container-$id" => 1 },
-            };
+            if ($max == 1) {
+                push @$field_list, "quantity-$id" => {
+                    type => 'Hidden',
+                    default => '1',
+                };
+            } else {
+                push @$field_list, "quantity-$id" => {
+                    type => 'Select',
+                    label => 'Quantity',
+                    tags => {
+                        hint => "You can request a maximum of " . NUMWORDS($max) . " containers",
+                        initial_hidden => 1,
+                    },
+                    options => [
+                        { value => "", label => '-' },
+                        map { { value => $_, label => $_ } } (1..$max),
+                    ],
+                    required_when => { "container-$id" => 1 },
+                };
+            }
             $c->cobrand->call_hook("bin_request_form_extra_fields", $service, $id, $field_list);
         }
     }
